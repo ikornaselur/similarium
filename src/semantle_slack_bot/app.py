a3d76fd231c6f0b321590263e3af8ec483022985
Axel Örn Sigurðsson <dev@absalon.is>
2022-04-20 22:10:00 +0100

    Simple proof of concept in place

--- src/semantle_slack_bot/app.py
+++ src/semantle_slack_bot/app.py
@@ -1,3 +1,4 @@
+import heapq
 import json
 import logging
 import os
@@ -5,7 +6,7 @@ import random
 import re
 from functools import lru_cache
 from pathlib import Path
-from typing import Awaitable, Callable
+from typing import Awaitable, Callable, Optional
 
 from rich.logging import RichHandler
 from slack_bolt.adapter.socket_mode.aiohttp import AsyncSocketModeHandler
@@ -13,7 +14,7 @@ from slack_bolt.app.async_app import AsyncApp
 
 from semantle_slack_bot.event_types import Body
 from semantle_slack_bot.model import get_model
-from semantle_slack_bot.utils import get_similarity
+from semantle_slack_bot.utils import get_custom_progress_bar, get_similarity
 
 FORMAT = "%(message)s"
 logging.basicConfig(
@@ -24,12 +25,19 @@ logging.basicConfig(
 )
 app = AsyncApp(token=os.environ["SLACK_BOT_TOKEN"])
 
-REGEX = re.compile(r"^\!(?P<guess>[a-z]+)$")
+REGEX = re.compile(r"^(?P<guess>[A-Za-z]+)$")
 
 with open(Path(__file__).parent / "target_words.json", "r") as f:
     target_words = json.load(f)
 
-GUESSES = {}
+Say = Callable[[str], Awaitable[dict]]
+Guess = tuple[float, Optional[int], int, str, str]
+
+TOP_GUESSES_TO_SHOW = 25
+LATEST_GUESSES_TO_SHOW = 3
+
+GUESSES: dict[str, dict[str, list[Guess]]] = {}
+GAMES = {}
 
 
 @lru_cache()
@@ -53,37 +61,168 @@ def get_progress_bar(amount: int, total: int) -> str:
     return f"{green_blocks}{white_blocks}"
 
 
+def get_thread_body(guesses: list[Guess]) -> str:
+    top_guesses = heapq.nlargest(TOP_GUESSES_TO_SHOW, guesses)
+    latest_guesses = heapq.nlargest(
+        LATEST_GUESSES_TO_SHOW,
+        [(line[2], line) for line in guesses],
+    )
+    message = "** Game 13 - April 20th **\n"
+
+    def guess_line(guess: Guess) -> str:
+        similarity, percentile, idx, word, user = guess
+        if percentile:
+            closeness = f"{get_progress_bar(percentile, 1000)} {percentile}/1000"
+        else:
+            closeness = "cold"
+
+        return f"{idx:>3}. {word:<10} {similarity:<8.02f} {closeness} <@{user}>\n"
+
+    # Show the latest guesses
+    message += "Latest guesses:\n"
+    for _, guess in latest_guesses:
+        message += guess_line(guess)
+
+    message += "--------\n"
+
+    for guess in top_guesses:
+        message += guess_line(guess)
+
+    return message
+
+
+def get_thread_blocks(guesses: list[Guess]) -> list:
+    blocks = [
+        {
+            "type": "header",
+            "text": {
+                "type": "plain_text",
+                "text": "Semantle day 2 - April 16th",
+                "emoji": True,
+            },
+        },
+        {"type": "divider"},
+    ]
+
+    top_guesses = heapq.nlargest(TOP_GUESSES_TO_SHOW, guesses)
+    latest_guesses = heapq.nlargest(
+        LATEST_GUESSES_TO_SHOW,
+        [(line[2], line) for line in guesses],
+    )
+
+    def guess_line(guess: Guess) -> tuple[str, str]:
+        similarity, percentile, idx, word, user = guess
+        if percentile:
+            closeness = (
+                f"{get_custom_progress_bar(percentile, 1000, width=6)} {percentile}/1000"
+            )
+        else:
+            closeness = "cold"
+
+        return (
+            f"`{idx:>3}. {similarity:<6.02f} {word:<12} <@{user}>`\n",
+            f"{closeness}\n",
+        )
+
+    latest_guesses_left_col = f"*Latest {LATEST_GUESSES_TO_SHOW} guesses*\n"
+    latest_guesses_right_col = ":p0:\n"
+    # Show the latest guesses
+    for _, guess in latest_guesses:
+        left, right = guess_line(guess)
+        latest_guesses_left_col += left
+        latest_guesses_right_col += right
+
+    blocks.append(
+        {
+            "type": "section",
+            "fields": [
+                {"type": "mrkdwn", "text": latest_guesses_left_col},
+                {"type": "mrkdwn", "text": latest_guesses_right_col},
+            ],
+        }
+    )
+    blocks.append({"type": "divider"})
+
+    top_guesses_left_col = f"*Top {TOP_GUESSES_TO_SHOW} guesses*\n"
+    top_guesses_right_col = ":p0:\n"
+
+    for guess in top_guesses:
+        left, right = guess_line(guess)
+        top_guesses_left_col += left
+        top_guesses_right_col += right
+
+    blocks.append(
+        {
+            "type": "section",
+            "fields": [
+                {"type": "mrkdwn", "text": top_guesses_left_col},
+                {"type": "mrkdwn", "text": top_guesses_right_col},
+            ],
+        }
+    )
+
+    return blocks
+
+
 @app.event("message")
-async def message(
-    body: Body, logger: logging.Logger, say: Callable[[str], Awaitable[None]]
-) -> None:
+async def message(body: Body, logger: logging.Logger, say: Say, client, ack) -> None:
+    async def _start_new_game() -> None:
+        resp = await say("Starting a new game!")
+        if resp.get("ok") is True:
+            logger.info("Starting a new game")
+            GAMES[resp["ts"]] = {
+                "guesses": [],
+            }
+
+    await ack()
+
     event = body["event"]
     channel = event["channel"]
     if channel not in GUESSES:
         GUESSES[channel] = {}
-    day = 1
+    day = 4
     secret = get_secret(channel, day=day)
-
     secret_vec = get_model(secret, secret)["vec"]
 
     if event["type"] == "message":
         message = event.get("text")
+        thread_ts = event.get("thread_ts")
 
-        if message and (match := REGEX.match(message)):
-            guess = match.group("guess")
+        if message == "!start":
+            await _start_new_game()
+        elif (
+            thread_ts
+            and message
+            and thread_ts in GAMES
+            and (match := REGEX.match(message))
+        ):
+            guess = match.group("guess").lower()
+            logger.info(f"Guess in a game: {guess}")
+
             result = get_model(secret, guess)
-            if percentile := result.get("percentile"):
-                closeness = f"{get_progress_bar(percentile, 1000)} {percentile}/1000"
-            else:
-                closeness = "cold"
+            if "vec" not in result:
+                logger.error("Invalid word")
+                return
 
+            percentile = result.get("percentile")
             similarity = get_similarity(secret_vec, result["vec"])
 
-            logger.info(
-                f"Got {guess=} which has {similarity=} {closeness=} (to {secret=})"
+            heapq.heappush(
+                GAMES[thread_ts]["guesses"],
+                (
+                    similarity,
+                    percentile,
+                    len(GAMES[thread_ts]["guesses"]) + 1,
+                    guess,
+                    event["user"],
+                ),
             )
 
-            await say(f"{guess:<10} {similarity:<8.02f} {closeness}")
+            await client.chat_update(
+                channel=event["channel"],
+                ts=thread_ts,
+                blocks=get_thread_blocks(GAMES[thread_ts]["guesses"]),
+            )
 
 
 async def main() -> None:
