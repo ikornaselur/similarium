df33ed299ecd035c0403904a6c97abfd29a32ed7
Axel Örn Sigurðsson <dev@absalon.is>
2022-05-10 00:01:54 +0100

    Hook up sentry integration

--- src/similarium/exceptions.py
+++ src/similarium/exceptions.py
@@ -1,3 +1,25 @@
+from asyncio.events import AbstractEventLoop
+from typing import Any
+
+import sentry_sdk
+
+from similarium.logging import logger
+
+
+def exception_handler(loop: AbstractEventLoop, context: dict[str, Any]) -> None:
+    exception = context.get("exception")
+    logger.error("Global exception handler got exception", exc_info=exception)
+
+    if exception:
+        sentry_sdk.capture_exception(exception)
+    else:
+        sentry_sdk.capture_message("Got unexpected asyncio exception")
+
+
+def init_exception_handler(loop: AbstractEventLoop):
+    loop.set_exception_handler(exception_handler)
+
+
 class SimilariumException(Exception):
     pass
 
--- src/similarium/tasks.py
+++ src/similarium/tasks.py
@@ -1,6 +1,8 @@
 import asyncio
 import datetime as dt
 
+import sentry_sdk
+
 from similarium import db
 from similarium.game import end_game, start_game
 from similarium.logging import logger
@@ -15,21 +17,25 @@ async def hourly_game_creator() -> None:
     posted and create games for each
     """
     while True:
-        sleep_time = get_seconds_left_of_hour()
-        logger.debug(f"Hourly task sleeping for {sleep_time:.0f} seconds")
-        await asyncio.sleep(sleep_time)
+        try:
+            sleep_time = get_seconds_left_of_hour()
+            logger.debug(f"Hourly task sleeping for {sleep_time:.0f} seconds")
+            await asyncio.sleep(sleep_time)
 
-        current_hour = dt.datetime.now(dt.timezone.utc).hour
+            current_hour = dt.datetime.now(dt.timezone.utc).hour
 
-        async with db.session() as session:
-            channels = await Channel.by_hour(current_hour, session=session)
+            async with db.session() as session:
+                channels = await Channel.by_hour(current_hour, session=session)
 
-        logger.debug(f"Ending games in {len(channels)} channels")
-        # End active games
-        await asyncio.gather(*[end_game(channel.id) for channel in channels])
+            logger.debug(f"Ending games in {len(channels)} channels")
+            # End active games
+            await asyncio.gather(*[end_game(channel.id) for channel in channels])
 
-        logger.debug(f"Starting games in {len(channels)} channels")
-        # Start new games
-        await asyncio.gather(*[start_game(channel.id) for channel in channels])
+            logger.debug(f"Starting games in {len(channels)} channels")
+            # Start new games
+            await asyncio.gather(*[start_game(channel.id) for channel in channels])
 
-        # TODO Timeouts?
+            # TODO Timeouts?
+        except Exception as e:
+            logger.error("Got exception in hourly task runner", exc_info=e)
+            sentry_sdk.capture_exception(e)
