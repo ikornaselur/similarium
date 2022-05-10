42e2bc53cc3da0ab7a6e1f14a90f1ddb13dac9b5
Axel Örn Sigurðsson <dev@absalon.is>
2022-05-10 21:29:07 +0100

    Init alembic

---
+++ alembic/env.py
@@ -0,0 +1,44 @@
+import asyncio
+from logging.config import fileConfig
+
+from alembic import context
+from sqlalchemy import engine_from_config, pool
+from sqlalchemy.ext.asyncio import AsyncEngine
+
+from similarium.config import config as similarium_config
+from similarium.db import Base
+from similarium.models import *  # noqa: F401
+
+config = context.config
+config.set_main_option("sqlalchemy.url", similarium_config.database.uri)
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+target_metadata = Base.metadata
+
+
+def do_run_migrations(connection):
+    context.configure(connection=connection, target_metadata=target_metadata)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_migrations_online():
+    connectable = AsyncEngine(
+        engine_from_config(
+            config.get_section(config.config_ini_section),
+            prefix="sqlalchemy.",
+            poolclass=pool.NullPool,
+            future=True,
+        )
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+
+asyncio.run(run_migrations_online())
