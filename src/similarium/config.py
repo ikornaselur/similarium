5de51ffd9154dd6fab2c39aafe469476f23b155c
Axel Örn Sigurðsson <dev@absalon.is>
2023-03-14 21:39:16 +0000

    Add basic support for env config

--- src/similarium/config.py
+++ src/similarium/config.py
@@ -1,4 +1,5 @@
 import dataclasses as dc
+import json
 import os
 from pathlib import Path
 from typing import Any, ClassVar, Protocol, TypeVar, cast, overload
@@ -91,6 +92,29 @@ def from_dict(klass: Any, d: Value) -> Value:
 
 
 def from_dict(klass: type[DataclassT], d: dict | Value) -> DataclassT | Value:
+    """Parse a dictionary into an instance of the given dataclass
+
+    For example:
+        @dc.dataclass
+        class SubConfig:
+            dorf: str
+
+        @dc.dataclass
+        class Config:
+            foo: str
+            bar: int
+            baz: list[str]
+            sub: SubConfig
+
+        config = from_dict(Config, {
+            "foo": "norf",
+            "bar": 123,
+            "baz": ["a", "b", "c"],
+            "sub": {"dorf": "x"},
+        })
+
+    will return an instance of `Config` with the values from the dict
+    """
     if not dc.is_dataclass(klass):
         return cast(Value, d)
     if not isinstance(d, dict):
@@ -102,6 +126,53 @@ def from_dict(klass: type[DataclassT], d: dict | Value) -> DataclassT | Value:
             f"Missing key(s) in {klass.__name__} section: {', '.join(missing)}"
         )
     return klass(**{f: from_dict(fieldtypes[f], d[f]) for f in d})
+
+
+def from_env(klass: type[DataclassT], base_name: str) -> DataclassT:
+    """Parse env into an instance of the given dataclass
+
+    For example:
+        @dc.dataclass
+        class SubConfig:
+            dorf: str
+
+        @dc.dataclass
+        class Config:
+            foo: str
+            bar: int
+            baz: list[str]
+            sub: SubConfig
+
+        config = from_env(Config, "ENV_EXAMPLE")
+
+    will return an instance of `Config` with values from the environment
+
+    In this example `from_env` will look for the following values:
+
+        * `ENV_EXAMPLE_FOO` -> Config.foo
+        * `ENV_EXAMPLE_BAR` -> Config.bar
+        * `ENV_EXAMPLE_BAZ` -> Config.baz
+        * `ENV_EXAMPLE_SUB__DORF` -> Config.sub.dorf
+
+    If the dataclass field type is not str, it's expected to be json loadable
+    """
+    fields = {}
+    for field in dc.fields(klass):
+        if dc.is_dataclass(field.type):
+            fields[field.name] = from_env(
+                field.type, f"{base_name}_{field.name}_".upper()
+            )
+        else:
+            env_var_name = f"{base_name}_{field.name}".upper()
+            if env_var_name not in os.environ:
+                raise MissingKey(
+                    f"Missing env var for {klass.__name__} section: {env_var_name}"
+                )
+            env_var = os.environ[env_var_name]
+            if field.type != str:
+                env_var = json.loads(env_var)
+            fields[field.name] = env_var
+    return klass(**fields)
 
 
 # Where to load the config from, supported options:
--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -1,8 +1,10 @@
 import dataclasses as dc
+import os
+from unittest import mock
 
 import pytest
 
-from similarium.config import MissingKey, from_dict
+from similarium.config import MissingKey, from_dict, from_env
 
 
 def test_from_dict_with_dataclass():
@@ -48,3 +50,50 @@ def test_from_dict_with_dataclass_missing_fields():
                 "foo": "baz",
             },
         )
+
+
+def test_from_env_with_dataclass():
+    @dc.dataclass
+    class Sub:
+        norf: str
+        lst: list[str]
+
+    @dc.dataclass
+    class Config:
+        foo: str
+        bar: str
+        baz: int
+        sub: Sub
+
+    with mock.patch.dict(
+        os.environ,
+        {
+            "TEST_FOO": "hello",
+            "TEST_BAR": "world",
+            "TEST_BAZ": "123",
+            "TEST_SUB__NORF": "sub_string",
+            "TEST_SUB__LST": '["1", "2", "3"]',
+        },
+    ):
+        config = from_env(Config, "TEST")
+
+    assert config.foo == "hello"
+    assert config.bar == "world"
+    assert config.baz == 123
+    assert config.sub.norf == "sub_string"
+    assert config.sub.lst == ["1", "2", "3"]
+
+
+def test_from_env_with_dataclass_missing_fields():
+    @dc.dataclass
+    class Config:
+        foo: str
+        bar: str
+
+    os.environ["TEST_FOO"] = "hello"
+
+    with pytest.raises(
+        MissingKey, match="Missing env var for Config section: TEST_BAR"
+    ):
+        with mock.patch.dict(os.environ, {"TEST_FOO": "hello"}):
+            from_env(Config, "TEST")
